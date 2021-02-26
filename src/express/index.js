20f8f934136a9840bb784229824fe22b4aa2ac72
max shapoval <kid.mbox@gmail.com>
2021-02-26 18:58:33 +0200

    add pug templates

--- src/express/index.js
+++ src/express/index.js
@@ -1,17 +1,26 @@
 'use strict';
 
 const express = require(`express`);
+const path = require(`path`);
 const mainRouter = require(`./routes/main`);
 const articlesRouter = require(`./routes/articles`);
 const myRouter = require(`./routes/my`);
 const chalk = require(`chalk`);
-const {HTTP_FRONT_SERVICE_PORT} = require(`../constants`);
+const {HTTP_FRONT_SERVICE_PORT, HTTP_CODES} = require(`../constants`);
 
 const app = express();
 
 app.use(`/`, mainRouter);
 app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
+
+app.use(express.static(path.resolve(__dirname, `public`)));
+
+app.use((req, res) => res.status(HTTP_CODES.NOT_FOUND).render(`layouts/404`));
+app.use((err, _req, res, _next) => res.status(HTTP_CODES.SERVER_ERROR).render(`layouts/500`));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(HTTP_FRONT_SERVICE_PORT, (err) => {
   if (err) {
